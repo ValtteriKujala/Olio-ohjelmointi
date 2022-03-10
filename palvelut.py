cbc37530cd908e5ac97a8d98e7d4605f058012b0
ValtteriKujala <82021326+ValtteriKujala@users.noreply.github.com>
2022-03-10 22:13:12 +0200

    Update palvelut.py

--- palvelut.py
+++ palvelut.py
@@ -1,14 +1,47 @@
+import random
+
 class Asiakas:
     def __int__(self, nimi, ika):
         self.__nimi = nimi
-        self.__asiakasnro = []
+        self.__asiakasnro = luo_nro()
         self.__ika = ika
 
-    def get_nimi():
-        pass
+    def __luo_nro(self):
+        kikel1 = random.randint(0, 9)
+        kikel2 = random.randint(0, 9)
+        kikel3 = random.randint(0, 9)
+        kikel4 = random.randint(0, 9)
+        kikel5 = random.randint(0, 9)
+        kikel6 = random.randint(0, 9)
+        kikel7 = random.randint(0, 9)
+        kikel8 = random.randint(0, 9)
+        return f'{kikel1}{kikel2}-{kikel3}{kikel4}{kikel5}-{kikel6}{kikel7}{kikel8}'   
 
-    def get_ika():
-        pass
+    def get_asiakasnumero(self):
+        return self.__asiakasnro
+ 
+    def get_nimi(self):
+        return self.__nimi
+ 
+    def get_ika(self):
+        return self.__ika
+    
+    def set_nimi(self, nimi2):
+        nimi2 = nimi2
+        if nimi2 != "":
+            raise ValueError("Uusi nimi kannattaa antaa UwU")
+        else:
+            self.__nimi = nimi2
+
+    def set_ika(self, ika2):
+        ika2 = ika2
+        if ika2 != "":
+            raise ValueError("Uusi ika kannattaa antaa UwU")
+        else:
+            self.__ika = ika2
+
+    def get_ika(self):
+        return self.__ika
 
     def __luo_nro():
         pass
