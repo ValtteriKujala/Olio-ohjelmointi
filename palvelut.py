25877fc791747bafac96d79c62665e87dcf0c007
ValtteriKujala <82021326+ValtteriKujala@users.noreply.github.com>
2022-03-10 21:01:21 +0200

    Update palvelut.py

--- palvelut.py
+++ palvelut.py
@@ -6,36 +6,3 @@ class Asiakas:
 
     def __luo_nro():
         pass
- 
-class Palvelu(Asiakas):
-    def __int__(self):
-        self.tuotenimi = tuotenimi
-        self.__asiakkaat = []
-
-    def _luo_asiakasrivi(Asiakas):
-        pass
-    
-    def lisaa_asiakasrivi(Asiakas):
-        pass
-
-    def poista_asiakas(Asiakas):
-        pass
-
-    def tulosta_asiakkaat():
-        pass
-    
-
-
-
-class ParempiPalvelu:
-    def __init__(self):
-        self.__edut = []
-
-    def lisaa_etu(__edut):
-        pass
-
-    def poista_etu(__edut):
-        pass
-
-    def tulosta_edut():
-        pass
