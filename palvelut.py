2d79d04c9fc73209e65b038ebbaead3b61ecb2a1
ValtteriKujala <82021326+ValtteriKujala@users.noreply.github.com>
2022-03-10 20:52:46 +0200

    Create palvelut.py

---
+++ palvelut.py
@@ -0,0 +1,38 @@
+class Asiakas:
+    def __int__(self):
+        self.__nimi = nimi
+        self.__asiakasnro = []
+        self.__ika = ika
+
+    def __luo_nro():
+        pass
+ 
+class Palvelu(Asiakas):
+    def __int__(self):
+        self.tuotenimi = tuotenimi
+        self.__asiakkaat = []
+
+    def _luo_asiakasrivi(Asiakas):
+        pass
+
+    def poista_asiakas(Asiakas):
+        pass
+
+    def tulosta_asiakkaat():
+        pass
+    
+
+
+
+class ParempiPalvelu:
+    def __init__(self):
+        self.__edut = []
+
+    def lisaa_etu(__edut):
+        pass
+
+    def poista_etu(__edut):
+        pass
+
+    def tulosta_edut():
+        pass
